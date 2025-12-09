e9fc2d454e5ab174377af3f1918930bfb61f1e39
Ryan Gupta <ryanngupta@gmail.com>
2025-12-08 18:02:29 -0600

    Initial Commit

---
+++ newzyxIOS/newzyxIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  newzyxIOSApp.swift
+//  newzyxIOS
+//
+//  Created by Ryan Gupta on 12/8/25.
+//
+
+import SwiftUI
+
+@main
+struct newzyxIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ newzyxIOSUITests/newzyxIOSUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  newzyxIOSUITestsLaunchTests.swift
+//  newzyxIOSUITests
+//
+//  Created by Ryan Gupta on 12/8/25.
+//
+
+import XCTest
+
+final class newzyxIOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
